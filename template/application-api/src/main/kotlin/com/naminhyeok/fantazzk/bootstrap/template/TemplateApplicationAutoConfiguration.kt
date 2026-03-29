407ab139716de4f1ffd33c7b473a5e437fac538f
NaMinhyeok <nmh9097@gmail.com>
2026-03-29 19:34:24 +0900

    feat(template): OpenAPI 빈 ConditionalOnMissingBean + GroupedOpenApi 등록

--- template/application-api/src/main/kotlin/com/naminhyeok/fantazzk/bootstrap/template/TemplateApplicationAutoConfiguration.kt
+++ template/application-api/src/main/kotlin/com/naminhyeok/fantazzk/bootstrap/template/TemplateApplicationAutoConfiguration.kt
@@ -2,16 +2,20 @@ package com.naminhyeok.fantazzk.bootstrap.template
 
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import java.time.Clock
 
 @AutoConfiguration
 class TemplateApplicationAutoConfiguration {
     @Bean
+    @ConditionalOnMissingBean
     fun clock(): Clock = Clock.systemUTC()
 
     @Bean
+    @ConditionalOnMissingBean(OpenAPI::class)
     fun openApi(): OpenAPI =
         OpenAPI()
             .info(
@@ -20,4 +24,11 @@ class TemplateApplicationAutoConfiguration {
                     .version("v1")
                     .description("팀 빌딩 템플릿 관리"),
             )
+
+    @Bean
+    fun templateGroupedOpenApi(): GroupedOpenApi =
+        GroupedOpenApi.builder()
+            .group("template")
+            .packagesToScan("com.naminhyeok.fantazzk.template")
+            .build()
 }
