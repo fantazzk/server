132a0ea05375db6abd2c00ed4edaaaed7765211a
NaMinhyeok <nmh9097@gmail.com>
2026-04-06 11:14:08 +0900

    refactor: strengthen room id value object

--- src/main/kotlin/com/naminhyeok/fantazzk/room/domain/RoomId.kt
+++ src/main/kotlin/com/naminhyeok/fantazzk/room/domain/RoomId.kt
@@ -4,4 +4,14 @@ import org.jmolecules.ddd.types.Identifier
 
 data class RoomId(
     val value: Long,
-) : Identifier
+) : Identifier {
+    init {
+        require(value > 0L) { "RoomId는 1 이상이어야 합니다" }
+    }
+
+    companion object {
+        fun from(value: Long): RoomId = RoomId(value)
+
+        fun fromOrNull(value: Long?): RoomId? = value?.takeIf { it != 0L }?.let(::from)
+    }
+}
