b01cdd5eb612085219be4f3f21a1c6ee532b7a21
NaMinhyeok <nmh9097@gmail.com>
2026-04-07 12:09:03 +0900

    feat: add template application services

---
+++ src/main/java/com/naminhyeok/fantazzk/template/FindTemplates.java
@@ -0,0 +1,23 @@
+package com.naminhyeok.fantazzk.template;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class FindTemplates {
+    private final Templates templates;
+
+    @Transactional(readOnly = true)
+    public TemplateDetail getDetail(Template.TemplateId id) {
+        Template template = templates.findById(id).orElseThrow(TemplateNotFoundException::new);
+        return new TemplateDetail(template, template.getPlayers());
+    }
+
+    @Transactional(readOnly = true)
+    public List<Template> list() {
+        return templates.findAll();
+    }
+}
