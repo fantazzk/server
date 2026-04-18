3aeeb3cae80ede47cc383ea9cc1ae78e7df35c6d
NaMinhyeok <nmh9097@gmail.com>
2026-04-18 13:13:27 +0900

    room web 요청 dto 가시성을 정리한다

--- src/main/java/com/naminhyeok/fantazzk/room/web/room/SelectDraftPositionRequest.java
+++ src/main/java/com/naminhyeok/fantazzk/room/web/room/SelectDraftPositionRequest.java
@@ -2,7 +2,7 @@ package com.naminhyeok.fantazzk.room.web.room;
 
 import jakarta.validation.constraints.NotNull;
 
-public record SelectDraftPositionRequest(
+record SelectDraftPositionRequest(
     @NotNull(message = "드래프트 자리는 필수입니다") Integer draftPosition
 ) {
 }
